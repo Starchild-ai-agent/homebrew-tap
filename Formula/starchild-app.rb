fb1bc1ed289356e7305c14311f1dacc509c33345
Aladdin <aladdin.starchild@proton.me>
2026-08-24 22:56:44 +0800

    starchild-app: 0.5.40 formula

--- Formula/starchild-app.rb
+++ Formula/starchild-app.rb
@@ -12,9 +12,9 @@ class StarchildApp < Formula
   homepage "https://iamstarchild.com"
   # Source tarball is published alongside the CLI by sc-chatroom
   # (route /starchild-app/<file>, served from tools/starchild-app/).
-  url "https://workroom.iamstarchild.com/starchild-app/starchild-app-0.5.39.tar.gz"
-  version "0.5.39"
-  sha256 "301eccf0471fd2dd4fdd10b44247367cdec77ebdc9fa59c54439336c0faf9c02"
+  url "https://workroom.iamstarchild.com/starchild-app/starchild-app-0.5.40.tar.gz"
+  version "0.5.40"
+  sha256 "b0049bfde0f02929488145f699c8383cc75af014d9ea4107d2ad6eccc5f1cfa7"
   license :cannot_represent
 
   depends_on "node" => :build
